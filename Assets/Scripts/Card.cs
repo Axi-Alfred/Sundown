9af116034a2ceb0a58ae0b8c235a7ae56b8e3e8e
Sadrinhooo <sadrabazi82@gmail.com>
2025-03-25 23:44:35 +0100

    Update Card.cs

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    private CardShape shape;
+    private int value;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,5 @@ public class Card : MonoBehaviour
 
 public enum CardShape
 {
-
+    Shape1, Shape2, Shape3, Shape4
 }
